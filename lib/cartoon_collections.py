5b381585fc59b720146ee9be7880785730482689
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-07-28 15:34:47 -0400

    Solution

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,30 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for index, name in enumerate(names, start= 1):
+        print (f"{index}. {name}")
+    
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    captain_list = []
+    for call in calls:
+        message = f"{call}".capitalize() + "!"
+        captain_list.append(message)
+    return captain_list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+     for call in calls:
+         if len(call) > 4:
+             return True
+     return False
 
-def find_the_cheese():
-    pass
+
+def find_the_cheese(items):
+    cheese_list = ["cheddar" , "gouda", "camembert"]
+
+    for item in items:
+        if item in cheese_list:
+            return item
+        
+    return None
+
+
+
